0ff82a31f5b3bae4febf92794c3408a6f7d257e2
matthewhutchings <matthewhutchings1@gmail.com>
2015-11-28 21:20:14 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,23 +12,23 @@ for name in train:
  
 html = requests.get('https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=southampton&scrambleSeed=833794509', headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"}).content
 		
-	dom = lxml.html.fromstring(html)
+dom = lxml.html.fromstring(html)
 		
-	premierLeagueData = []
+premierLeagueData = []
 		
-	x = 1
-		
-		
+x = 1
 		
 		
-	for row in dom.cssselect('div.businessCapsule-fle'):
+		
+		
+for row in dom.cssselect('div.businessCapsule-fle'):
 		    
-		    x += 1
+	x += 1
 		    
-		    id = x
-		    name = str(row.cssselect('div.businessCapsule--title')[0].text_content())
-		    telephone = str(row.cssselect('div.businessCapsule--telephone')[0].text_content())
-		    address = str(row.cssselect('div.businessCapsule--address')[0].text_content())
+		id = x
+		name = str(row.cssselect('div.businessCapsule--title')[0].text_content())
+		telephone = str(row.cssselect('div.businessCapsule--telephone')[0].text_content())
+		address = str(row.cssselect('div.businessCapsule--address')[0].text_content())
 		       
 		    
 		    teamItem = {
@@ -36,11 +36,11 @@ html = requests.get('https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&
 		    'name':name,
 		    'telephone':telephone,
 		    'address':address
-		     }
+			}
 		    
 		    premierLeagueData.append(teamItem)
 		
-		if len(premierLeagueData) > 0:
+			if len(premierLeagueData) > 0:
 		    #truncate data store
 		    scraperwiki.sql.execute("DROP TABLE IF EXISTS `data`")
 		    #add each table line to data store

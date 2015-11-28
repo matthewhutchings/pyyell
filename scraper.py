a5b60899e9b70dc2199731a98aad57afe75e9ebd
matthewhutchings <matthewhutchings1@gmail.com>
2015-11-28 16:24:27 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@ import requests
 import lxml.html
 import scraperwiki
 
+
+
 html = requests.get('https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=southampton&scrambleSeed=833794509').content
 
 dom = lxml.html.fromstring(html)
@@ -16,22 +18,10 @@ for row in dom.cssselect('div.businessCapsule-fle'):
     pos = int(row.cssselect('div.col-sm-24')[0].text_content())
     
     
+       
     
-    team = row.cssselect('.col-club')[0].text_content()
-    goalsFor = int(row.cssselect('.col-gf')[0].text_content())
-    goalsAgainst = int(row.cssselect('.col-ga')[0].text_content())
-    goalDifference = int(row.cssselect('.col-gd')[0].text_content())
-    points = int(row.cssselect('.col-pts')[0].text_content())
+    teamItem = {'pos':pos}
     
-    print pos, team,"gf", goalsFor, "ga", goalsAgainst, "gd", goalDifference, "pts", points
-    
-    
-    teamItem = {'pos':pos,
-        'team':team,
-        'gf':goalsFor,
-        'ga':goalsAgainst,
-        'gd':goalDifference,
-        'pts':points}
     premierLeagueData.append(teamItem)
 
 if len(premierLeagueData) > 0:

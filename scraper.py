ef342a36a07f7a93e4a1ed858a5193935fc397cb
matthewhutchings <matthewhutchings1@gmail.com>
2015-11-28 21:06:15 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,38 +3,47 @@ import requests
 import lxml.html
 import scraperwiki
 
-html = requests.get('https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=southampton&scrambleSeed=833794509', headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"}).content
 
-dom = lxml.html.fromstring(html)
 
-premierLeagueData = []
+for x in range(0, 3):
+    print "We're on time %d" % (x)
 
-x = 1
-
-for row in dom.cssselect('div.businessCapsule-fle'):
-    
-    x += 1
-    
-    id = x
-    name = str(row.cssselect('div.businessCapsule--title')[0].text_content())
-    telephone = str(row.cssselect('div.businessCapsule--telephone')[0].text_content())
-    address = str(row.cssselect('div.businessCapsule--address')[0].text_content())
-       
-    
-    teamItem = {
-    'id':id,
-    'name':name,
-    'telephone':telephone,
-    'address':address
-     }
-    
-    premierLeagueData.append(teamItem)
-
-if len(premierLeagueData) > 0:
-    #truncate data store
-    scraperwiki.sql.execute("DROP TABLE IF EXISTS `swdata`")
-    #add each table line to data store
-    for teamItem in premierLeagueData:
-        scraperwiki.sql.save(['id'], teamItem)
+ 
+		html = requests.get('https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=southampton&scrambleSeed=833794509', headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"}).content
+		
+		dom = lxml.html.fromstring(html)
+		
+		premierLeagueData = []
+		
+		x = 1
+		
+		
+		
+		
+		for row in dom.cssselect('div.businessCapsule-fle'):
+		    
+		    x += 1
+		    
+		    id = x
+		    name = str(row.cssselect('div.businessCapsule--title')[0].text_content())
+		    telephone = str(row.cssselect('div.businessCapsule--telephone')[0].text_content())
+		    address = str(row.cssselect('div.businessCapsule--address')[0].text_content())
+		       
+		    
+		    teamItem = {
+		    'id':id,
+		    'name':name,
+		    'telephone':telephone,
+		    'address':address
+		     }
+		    
+		    premierLeagueData.append(teamItem)
+		
+		if len(premierLeagueData) > 0:
+		    #truncate data store
+		    scraperwiki.sql.execute("DROP TABLE IF EXISTS `data`")
+		    #add each table line to data store
+		    for teamItem in premierLeagueData:
+		        scraperwiki.sql.save(['id'], teamItem)
 
 

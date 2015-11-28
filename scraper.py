18fc6d70bc1f9248cc6c0f0754c20add74ff83f8
matthewhutchings <matthewhutchings1@gmail.com>
2015-11-28 16:17:26 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,20 +3,29 @@ import requests
 import lxml.html
 import scraperwiki
 
-html = requests.get('http://www.premierleague.com/en-gb/matchday/league-table.html').content
+html = requests.get('https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=southampton&scrambleSeed=833794509').content
 
 dom = lxml.html.fromstring(html)
 
 premierLeagueData = []
 
-for row in dom.cssselect('tr.club-row'):
-    pos = int(row.cssselect('.col-pos')[0].text_content())
+for row in dom.cssselect('div.businessCapsule-fle'):
+    
+    
+    
+    pos = int(row.cssselect('div.col-sm-24')[0].text_content())
+    
+    
+    
     team = row.cssselect('.col-club')[0].text_content()
     goalsFor = int(row.cssselect('.col-gf')[0].text_content())
     goalsAgainst = int(row.cssselect('.col-ga')[0].text_content())
     goalDifference = int(row.cssselect('.col-gd')[0].text_content())
     points = int(row.cssselect('.col-pts')[0].text_content())
-    #print pos, team,"gf", goalsFor, "ga", goalsAgainst, "gd", goalDifference, "pts", points
+    
+    print pos, team,"gf", goalsFor, "ga", goalsAgainst, "gd", goalDifference, "pts", points
+    
+    
     teamItem = {'pos':pos,
         'team':team,
         'gf':goalsFor,

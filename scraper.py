5202726541cfe0941b307aa4d38818959c213b0d
matthewhutchings <matthewhutchings1@gmail.com>
2015-11-29 09:37:37 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,6 +11,8 @@ for name in train:
 	
 	url = 'https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=southampton&scrambleSeed=833794509&pageNum'+name  
  
+ 	print url
+ 	
  	html = requests.get(url, headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"}).content
 			
 	dom = lxml.html.fromstring(html)

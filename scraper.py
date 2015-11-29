7920c80da6f302c45fad381809150050f53af30e
matthewhutchings <matthewhutchings1@gmail.com>
2015-11-29 19:09:57 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,15 +6,15 @@ import scraperwiki
 
 train = "1", "2", "3"
 for name in train:
-	print name
-	
+
 	url = 'http://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=southampton&scrambleSeed=833794509&pageNum='+name  
  
- 	print name
- 	
+
  	html = requests.get(url, headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"}).content
 	dom = lxml.html.fromstring(html)
 	premierLeagueData = []
+	
+	print html
 	
 	x = 1
 					
@@ -25,8 +25,6 @@ for name in train:
 			    
 		id = x
 		name = str(row.cssselect('div.businessCapsule--title')[0].text_content())
-		
-		print name 
 		
 		telephone = str(row.cssselect('div.businessCapsule--telephone')[0].text_content())
 		address = str(row.cssselect('div.businessCapsule--address')[0].text_content())

e67042dac17aa26e321adeff5b3e051df20657b2
matthewhutchings <matthewhutchings1@gmail.com>
2015-11-29 09:48:18 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,10 +16,11 @@ for name in train:
  	
  	html = requests.get(url, headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"}).content
 	
-	print requests.headers
 	
 	dom = lxml.html.fromstring(html)
-			
+	
+	print dom.headers
+		
 	premierLeagueData = []
 			
 	x = 1
